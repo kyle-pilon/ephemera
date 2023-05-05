e93c902a17d80cca916f687fe33fdec0c7b9945c
Kyle Pilon <pilonk@rpi.edu>
2023-05-05 16:58:17 -0400

    setup

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/no-unescaped-entities */
+export default function About() {
+  return (
+    <div>
+      <h1>About Ephemera</h1>
+      <p>
+        Ephemera is a company specializing in surreal, psychedelic, and trippy shirts and merch.
+        We're passionate about creating unique and eye-catching designs for our customers.
+      </p>
+      {/* Add more content and styling as needed */}
+    </div>
+  );
+};
---
+++ app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Explore our wide range of products.</p>
+      {/* Add product listings and styling once you have your Shopify store and products set up with Printify */}
+    </div>
+  );
+};
+
